e462e491232c6cb3ca55183f2b9cd8b0d6c8705e
Nikita Kolmogorov <backmeupplz@gmail.com>
2018-12-11 12:21:17 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const token = '612475171:AAFUZHwocef3w5vLF5lYR3ti8XRS1alKrXA';
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', (msg) => {
+  if (msg && msg.text && msg.text === 'пидор') {
+    return;
+  }
   if (!msg.sticker) {
     bot.deleteMessage(msg.chat.id, msg.message_id);
   }
